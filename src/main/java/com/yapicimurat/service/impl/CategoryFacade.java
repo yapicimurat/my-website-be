0c5e35840140d2ffcb3b920ca9c43c1cc9ad9ae0
yapicimurat <muratyapiicii@gmail.com>
2023-10-10 00:14:26 +0300

    Add DataResponse Wrapper for responses

--- src/main/java/com/yapicimurat/service/impl/CategoryFacade.java
+++ src/main/java/com/yapicimurat/service/impl/CategoryFacade.java
@@ -2,13 +2,14 @@ package com.yapicimurat.service.impl;
 
 import com.yapicimurat.controller.request.CategoryCreateRequest;
 import com.yapicimurat.controller.request.CategoryUpdateRequest;
+import com.yapicimurat.controller.response.DataResponse;
+import com.yapicimurat.controller.response.SuccessDataResponse;
 import com.yapicimurat.dto.CategoryDTO;
 import com.yapicimurat.service.CategoryService;
 import com.yapicimurat.util.ConverterUtil;
 import com.yapicimurat.util.GeneralUtil;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.UUID;
 
@@ -23,45 +24,43 @@ public class CategoryFacade {
         this.modelMapper = modelMapper;
     }
 
-    public List<CategoryDTO> getByIdList(final List<String> categoryIdList) {
+    public DataResponse<List<CategoryDTO>> getByIdList(final List<String> categoryIdList) {
         final List<UUID> uuidList = ConverterUtil.stringListToUUIDList(categoryIdList);
         final List<CategoryDTO> categoryDTOList = GeneralUtil
                 .mapEntityListToDTOList(categoryService.getByIdList(uuidList), CategoryDTO.class, modelMapper);
 
-        return categoryDTOList;
+        return new SuccessDataResponse<>(categoryDTOList, "");
     }
 
-    public List<CategoryDTO> getAll() {
+    public DataResponse<List<CategoryDTO>> getAll() {
         final List<CategoryDTO> categoryDTOList = GeneralUtil
                 .mapEntityListToDTOList(categoryService.getAll(), CategoryDTO.class, modelMapper);
 
-        return categoryDTOList;
+        return new SuccessDataResponse<>(categoryDTOList, "");
     }
 
-    public CategoryDTO getById(String id) {
+    public DataResponse<CategoryDTO> getById(String id) {
         final CategoryDTO categoryDTO = modelMapper.map(categoryService.getById(UUID.fromString(id)), CategoryDTO.class);
 
-        return categoryDTO;
+        return new SuccessDataResponse<>(categoryDTO, "");
     }
 
-    public List<String> createAll(List<CategoryCreateRequest> requestBody) {
+    public DataResponse<List<String>> createAll(List<CategoryCreateRequest> requestBody) {
         final List<String> createdCategoryIdList = ConverterUtil.uuidListToStringList(categoryService.createAll(requestBody));
 
-        return createdCategoryIdList;
+        return new SuccessDataResponse<>(createdCategoryIdList, "");
     }
 
-    public String create(CategoryCreateRequest requestBody) {
+    public DataResponse<String> create(CategoryCreateRequest requestBody) {
         final String createdCategoryId = categoryService.create(requestBody).toString();
 
-        return createdCategoryId;
+        return new SuccessDataResponse<>(createdCategoryId, "");
     }
 
-    public String updateById(String id, CategoryUpdateRequest requestBody) {
+    public DataResponse<String> updateById(String id, CategoryUpdateRequest requestBody) {
          final UUID updatedCategoryId = categoryService.updateById(UUID.fromString(id), requestBody);
 
-         return updatedCategoryId.toString();
+         return new SuccessDataResponse<>(updatedCategoryId.toString(), "");
     }
-
-
 
 }
