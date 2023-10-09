1a006b456ecf717cd5e356ea68144a5b675b8417
yapicimurat <muratyapiicii@gmail.com>
2023-10-10 00:14:44 +0300

    Add null check to fix null pointer exception

--- src/main/java/com/yapicimurat/util/ConverterUtil.java
+++ src/main/java/com/yapicimurat/util/ConverterUtil.java
@@ -1,6 +1,8 @@
 package com.yapicimurat.util;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -10,10 +12,12 @@ public final class ConverterUtil {
     }
 
     public static List<UUID> stringListToUUIDList(List<String> stringIdList) {
+        if(Objects.isNull(stringIdList)) return Collections.emptyList();
         return stringIdList.stream().map(UUID::fromString).collect(Collectors.toList());
     }
 
     public static List<String> uuidListToStringList(List<UUID> uuidList) {
+        if(Objects.isNull(uuidList)) return Collections.emptyList();
         return uuidList.stream().map(UUID::toString).collect(Collectors.toList());
     }
 }
