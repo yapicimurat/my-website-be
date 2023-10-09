6b995546d7820b0d0ca96ac0b247853ddfaadcca
yapicimurat <muratyapiicii@gmail.com>
2023-10-10 00:15:58 +0300

    Create a constant for exception messages

---
+++ src/main/java/com/yapicimurat/constant/ExceptionConstant.java
@@ -0,0 +1,6 @@
+package com.yapicimurat.constant;
+
+public final class ExceptionConstant {
+    private ExceptionConstant() {throw new AssertionError();}
+    public static final String ENTITY_NOT_FOUND = "Veri bulunamadı.";
+}
